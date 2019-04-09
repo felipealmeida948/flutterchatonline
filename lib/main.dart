e9c483ecd5ae6e077a91fb3fdf018b6b1c3a811f
Felipe Almeida <felipealmeida@Felipes-iMac.lan>
2019-04-09 00:24:24 -0300

    Ajustes de projeto

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,11 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'dart:async';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
 
 void main() async {
   runApp(MyApp());
@@ -14,6 +20,46 @@ final ThemeData kDefaultTheme = ThemeData(
   primarySwatch: Colors.purple,
   accentColor: Colors.deepOrangeAccent[400],
 );
+
+final googleSingIn = GoogleSignIn(); 
+final auth = FirebaseAuth.instance; 
+
+Future<Null> _ensureLoggedIn() async{
+  GoogleSignInAccount user = googleSingIn.currentUser; 
+  
+  if(user == null)
+    user = await googleSingIn.signInSilently();
+
+  if(user == null )
+    user = await googleSingIn.signIn(); 
+
+  if(await auth.currentUser() == null){
+    GoogleSignInAuthentication credentials = await googleSingIn.currentUser.authentication;
+
+    await auth.signInWithCredential(GoogleAuthProvider.getCredential(
+      idToken: credentials.idToken,
+      accessToken: credentials.accessToken
+      )
+    );
+  
+  }
+}
+
+_handleSubmitted(String text) async{
+  await _ensureLoggedIn(); 
+  _sendMenssage(text: text);
+}
+
+void _sendMenssage({String text, String imgUrl}){
+  Firestore.instance.collection("messages").add(
+    {
+      "text": text,
+      "imgUrl": imgUrl,
+      "senderName": googleSingIn.currentUser.displayName, 
+      "senderPhotoUrl": googleSingIn.currentUser.photoUrl
+    }
+  );
+}
 
 class MyApp extends StatelessWidget {
   @override
@@ -50,6 +96,18 @@ class _ChatScreenState extends State<ChatScreen> {
         ),
         body: Column(
           children: <Widget>[
+            Expanded(
+              child: ListView(
+                children: <Widget>[
+                  ChatMessage(),
+                  ChatMessage(),
+                  ChatMessage(),
+                ],
+              ),
+            ),
+            Divider(
+              height: 1.0,
+            ),
             Container(
               decoration: BoxDecoration(
                 color: Theme.of(context).cardColor,
@@ -72,6 +130,10 @@ class TextComposer extends StatefulWidget {
 }
 
 class _TextComposerState extends State<TextComposer> {
+  final _textController = TextEditingController();  
+  
+  bool _isComposing = false;
+
   @override
   Widget build(BuildContext context) {
     return IconTheme(
@@ -92,12 +154,72 @@ class _TextComposerState extends State<TextComposer> {
             ),
             Expanded(
               child: TextField(
+                controller: _textController,
                 decoration:
                     InputDecoration.collapsed(hintText: "Enviar uma messagem"),
+                onChanged: (text) {
+                  setState(() {
+                    _isComposing = text.length > 0;
+                  });
+                },
+                onSubmitted: (text){
+                  _handleSubmitted(text);
+                },
               ),
-            )
+            ),
+            Container(
+              margin: const EdgeInsets.symmetric(horizontal: 4.0),
+              child: Theme.of(context).platform == TargetPlatform.iOS
+                  ? CupertinoButton(
+                      child: Text("Enviar"),
+                      onPressed: _isComposing ? () {
+                        _handleSubmitted(_textController.text);
+                      } : null,
+                    )
+                  : IconButton(
+                      icon: Icon(Icons.send),
+                      onPressed: () {
+                        _handleSubmitted(_textController.text);
+                      },
+                    ),
+            ),
           ],
         ),
+      ),
+    );
+  }
+}
+
+class ChatMessage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 10.0),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Container(
+            margin: const EdgeInsets.only(right: 16.0),
+            child: CircleAvatar(
+              backgroundImage: NetworkImage("https://www.redwolf.in/image/catalog/artwork-Images/mens/iron-man-mask-design-image'.png"),
+            ),
+          ),
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(
+                  "Felipe",
+                  style: Theme.of(context).textTheme.subhead,
+                ),
+                Container(
+                  margin: const EdgeInsets.only(top: 5.0),
+                  child: Text("Test"),
+                )
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
